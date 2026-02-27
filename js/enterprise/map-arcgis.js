0423743bdc9cdbf57425fa3d1ca76230305b293c
joshdels <deleonjoshy2@gmail.com>
2026-02-27 22:27:51 +0800

    feat: clickable static buttons

--- js/enterprise/map-arcgis.js
+++ js/enterprise/map-arcgis.js
@@ -15,19 +15,19 @@
   const view = new MapView({
     container: "map-arcgis",
     map: map,
-    zoom: 6,
+    zoom: 11,
     center: [-1.3, 50.7],
   });
 
-  const roadsLayer = new FeatureLayer({
-    // Change this data if load
-    url: "https://services1.arcgis.com/6677msI40mnLuuLr/arcgis/rest/services/Isle_of_Wight_WFL1/FeatureServer/5",
-    outFields: ["*"],
-    popupEnabled: true,
-    visible: true,
-  });
+  // const roadsLayer = new FeatureLayer({
+  //   // Change this data if load
+  //   url: "https://services1.arcgis.com/6677msI40mnLuuLr/arcgis/rest/services/Isle_of_Wight_WFL1/FeatureServer/5",
+  //   outFields: ["*"],
+  //   popupEnabled: true,
+  //   visible: true,
+  // });
 
-  map.add(roadsLayer);
+  // map.add(roadsLayer);
 
   const townsLayer = new GeoJSONLayer({
     url: "data/iow_town.geojson",
@@ -55,16 +55,16 @@
   const legend = new Legend({
     view: view,
     layerInfos: [
-      { layer: roadsLayer, title: "Roads" },
+      // { layer: roadsLayer, title: "Roads" },
       { layer: townsLayer, title: "Towns" },
     ],
     container: "legendDiv",
   });
 
   try {
-    await roadsLayer.when();
-    const layerView = await view.whenLayerView(roadsLayer);
-    const result = await roadsLayer.queryExtent();
+    await townsLayer.when();
+    const layerView = await view.whenLayerView(townsLayer);
+    const result = await townsLayer.queryExtent();
 
     if (result.extent) {
       view.goTo(result.extent, { duration: 1500 });
