da556c33e557aa69e730859b931e8bff88125bf5
joshdels <assistantgisjosh@gmail.com>
2026-03-05 18:34:52 +0800

    feat: return home/hide togglers

--- js/enterprise/map-arcgis.js
+++ js/enterprise/map-arcgis.js
@@ -85,4 +85,10 @@
   } catch (error) {
     console.error("Error zooming to layer:", error);
   }
+
+  const toggleBtn = document.getElementById("toggle-legend");
+  toggleBtn.addEventListener("click", () => {
+    const div = document.getElementById("legendDiv");
+    div.style.display = div.style.display === "none" ? "block" : "none";
+  });
 })();
